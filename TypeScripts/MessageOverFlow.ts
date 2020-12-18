ead41682dac11b378a3b6c8b6ca160e8a22f670c
Pieter <pieterbrandsen2@gmail.com>
2020-12-18 12:01:30 +0100

    Update MessageOverFlow.ts
    Hiervoor was het 100% javascript
    
    Kleine aanpassing om toch nog iets van typescript te gebruiken
    
    Heb de functie verkort en void als return type.

--- TypeScripts/MessageOverFlow.ts
+++ TypeScripts/MessageOverFlow.ts
@@ -1,5 +1,1 @@
-﻿document.getElementById("LastButtonId").onclick =
-    function ()
-    {
-        alert("The limit is 5 different persons on each mail that's send");
-    }
+﻿document.getElementById("LastButtonId").onclick = (): void => alert("The limit is 5 different persons on each mail that's send");
